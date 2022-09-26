2961b4adef712e499bf22e790ad0482a0ba21585
Dave Fisher <dave.j.fish@gmail.com>
2022-09-26 16:42:48 -0700

    navlinks and routes working

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import { Switch, Route, NavLink } from 'react-router-dom';
+import { Switch, Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
 import Color from '../Color/Color';
 import Home from '../Home/Home';
 import NotFound from '../NotFound/NotFound';
@@ -7,12 +7,27 @@ import './Main.css';
 
 export default function Main() {
   return (
-    <div className='main'>
-      <Switch>
-        <Route exact path='/'>{Home}</Route>
-        <Route path='/:r/:g/:b'>{Color}</Route>
-        <Route path='*'>{NotFound}</Route>
-      </Switch>
-    </div>
+    <Router>
+      <header>
+        <ul>
+          <NavLink className='red' to='/rgb/166/28/60'>
+            Vivid Burgundy
+          </NavLink>
+          <NavLink className='green' to='/rgb/58/183/149'>
+            Mountain Meadow
+          </NavLink>
+          <NavLink className='blue' to='/rgb/104/116/232'>
+            Medium Slate Blue
+          </NavLink>
+        </ul>
+      </header>
+      <div className='main'>
+        <Switch>
+          <Route exact path='/'>{Home}</Route>
+          <Route path='/:r/:g/:b'>{Color}</Route>
+          <Route path='*'>{NotFound}</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
